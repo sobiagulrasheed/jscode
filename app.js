6843840e739f80167ba6dd7ca5ce72f07adfca1a
sobiagulrasheed <sobiagulrasheed@gmail.com>
2020-06-14 15:11:13 -0700

    jscode

---
+++ app.js
@@ -0,0 +1,206 @@
+// alert("hello world")
+// var a = "sobia"
+// var b = "areej"
+// a = "hassan"
+// let c = "salman"
+// const d = "5"
+// alert ("Error! please enter a valid password")
+// alert ("Welcome to JS Land...\nHappy Coding!")
+// alert ("Welcome to JS Land...\nHappy Coding!\ni love my Pakistan")
+// alert ("Welcome to JS Land...")
+// alert ("Happy Coding...!")
+// var username = "sobia"
+// var myname = "15 years"
+// var certi = "certified mobile application developer"
+// alert(username)
+// alert(myname)
+// alert(certi)
+// alert("pizza\npizz\npiz\npi\npi\np")
+// alert ("my email address is " + " abc@gmail.com")
+// var book = "I m  Learning JS "
+// alert( book + "A samrter way to learn javasript")
+// var a ="Yah i can write HTML content through java script"
+//     document.write(a);
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert("im 15 years old")
+// var num = "you have visited this site 14 times"
+// alert(num)
+// var b ="my birth year is 1990 <br> data type of my declared number is"
+//     document.write(b);
+// var username = prompt("Please enter your name", "");
+// var product = prompt("Please enter your product", "");
+// var quantity = prompt ("how many t sirts do you want", "")
+// document.write(username + "  ordered  " + quantity + "      ");
+// var $my_1stvariable = 2 ;
+// var $name,_name = 4 ;
+// var name = 6 ;
+// document.write("<h1>Rules for naming JS variables</h1><br><br><br>Variable names can only contain numbers $, and _ . For example $my_1stVariable<br>Variables must begin with a $ or _. For example $name, _name or name<br>Variable names are case sensitive<br>Variable names should not be JS keywords");
+// var c =3
+//   var d =5
+// document.write(" sum of " + c + " and " + d + " is " + (c+d)+ "<br>");
+
+// document.write(" substraction of " + c + " and " + d + " is " + (c-d)+ "<br>");
+
+// document.write(" multipilication of " + c + " and " + d + " is " + (c*d)+ "<br>");
+// document.write(" Division of " + c + " and " + d + " is " + (c/d)+ "<br>");
+// document.write(" module of " + c + " and " + d + " is " + (c%d)+ "<br>");
+// var  e = "?";
+// document.write("Value after variable declaration is " + e + "<br>");
+// var f = 5
+// document.write("Initial value:  " + f + "<br>");
+// document.write("Value after increment is:  " + ++f + "<br>");
+// var g = 7
+// var h = 6
+// var f = (g + h)
+// document.write("Value after addition  is:  " + (g + h)+ "<br>");
+// var f = --f
+// document.write("Value after addition  is:  " + f + "<br>");
+// document.write("The Reminder  is:  " + f%3 + "<br>");
+// var i = 600
+// document.write("total cost to buy a 5 tikets to a movie is :  " + i*5 + " PKR" + "<br>");
+// for(var i = 0; i < 10; i++ <br>){
+//   document.write(i)
+// };
+// document.write("<h1>Table of 2</h1><br> ")
+// for(var i = 1; i<= 10; i++){
+    
+//     document.write("2" + "x" + i + "=" + 2*i + "<br>")
+// }
+
+// var c = +prompt("enter temp")
+// var f = +prompt("enter temp in far")
+// var cel =(c-32)*5/9
+// var far = (f*9/5)+32
+// document.write("temp in celsius is" +cel + "<br>")
+// document.write("temp in farenhtesis is" +far)
+
+// var a = 650
+// var b = 100
+// var c = 100
+// var e = +prompt("Quantity of item1")
+// var f = +prompt("Quantity of item2")
+// var result = (a*e)+(b*f)+c
+// document.write("<h1> Shopping Cart </h1> <br>" + "Price of item 1 " + a + "<br>" + "quantity of item 1 is " + e + "<br>" + "Price of item 2 is " + b + "<br>" + "quantity of item 2 is "+ f + "<br>"+ result )
+
+// var a = 980
+// var b = 804
+// document.write("pecentage is " + b/a*100)
+//  var pkr = +prompt("pakistani rupees")
+//  var riyal = (pkr*28)
+//  document.write("total currency is" + riyal)
+
+// var a = 5
+// var b = a*10/2
+// document.write(b)
+
+// var e = +prompt("current year")
+// var f = +prompt("birth year")
+// var r = (e - f)
+// document.write("current year" + e + "<br>" + "birth year" + f +  "<br>" + r )
+
+// var e = +prompt("Radius of circle is")
+// var f = 2*3.14*e
+// var g = 3.14*2*2
+// document.write("radius of circle is " + e  + "<br>"+"The circumference is" + f + "<br>"+ "Area of a circle" + g)
+
+// Chapter no 6-9
+// var a = 10
+// document.write("the value of a is: " + a +"<br>" + "the value of ++a is :" + ++a +"<br>" + "Now the value of A is  :" + a++ +"<br>" + "the value of a++ is :" + a
+// +"<br>" + "the value of --a is :" + --a +"<br>" + "the value of --a is :" + --a)
+
+// var a = 2,b=1;
+// var result = --a - --b + ++b + b--
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+
+// document.write("a is " + a + "<br>" + "b is " + b + "<br>" + "the result is"  + result)
+
+// var a = prompt("Write Your Name")
+// alert( a+ " welcome to our page")
+
+// var e = +prompt("write a number ",5)
+// for(var i = 1; i<= 10; i++){
+// document.write(e + "x" + i + "=" + e*i + "<br>")
+//  }
+
+// var  subject1 = prompt("write your subject")
+// var  subject2 = prompt("write your subject")
+// var  subject3 = prompt("write your subject")
+// var total_marks = 100
+// var  obt1 = prompt("write your Obtain marks")
+// var  obt2 = prompt("write your obtain Mrks")
+// var  obt3 = prompt("write your Obtain Marks")
+
+// var percentage1 = obt1/subject1*100
+// var percentage2 = obt2/subject1*100
+// var percentage3 = obt3/subject1*100
+
+// var totalsubjects = subject1 + subject2 + subject3
+// var totaltotal = total_marks *3;
+// var percentagetotal= percentage1 + percentage2 + percentage3;
+// document.write('<br/>'+ 'English'
+//  + "    " +
+// 'Total Marks' + "    " + 
+// 'Obtained Marks' + "    " + 'Percenatge'); 
+// document.write('<br/>' +subject1 + "           " + total_marks + "         " + obt1 + "           " + percentage1 + "%");
+// document.write('<br/>' +subject2 + "           " + total_marks + "          " + obt2 + "           " + percentage2+ "%");
+// document.write('<br/>' +subject3 + "           " + total_marks + "          " + obt3 + "           " + percentage3+ "%");
+// document.write('<br/>' + "         "  + totaltotal+ "       " + totalsubjects + "      " + percentagetotal + "%");
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+// var city  = prompt("write your city name") 
+// if (city == "karachi"){
+// document.write("welcome to the city of lights")}
+
+// var gender = prompt("what is you gender")
+// if (gender == "male"){
+// document.write("good morning sir")}
+// else if (gender == "female")
+// {document.write("good morning madam")}
+
+// var color = prompt("what is your signal color")
+// if (color == "red"){
+// document.write("must stop")}
+// else if (color == "yellow")
+// {document.write("ready to move")}
+// else if (color == "green")
+// {document.write("move now")}
+
+// var feul = +prompt("write your current fuel ?")
+//  if (feul  <500){
+//  document.write("recharge your fuel")}
+
+var  total_marks = 300
+var  marks_obtained = prompt("enter your marks")
+var  percentage = total_marks/marks_obtained*100
+ if (percentage >=80){
+document.write("<h1> Mark Sheet </h1>" + "<br>" + "total Marks" +  total_marks + "<br>" +  "obtain Marks" 
++ marks_obtained + "<br>" 
++ " Grade : your grade is A - One" + "<br>" + " Remark :you are excellent" 
++ "<br>" + "Your Pecentage is " + percentage)}
+else if (percentage >=70){
+    document.write("<h1> Mark Sheet </h1>" + "<br>" + "total Marks" +  total_marks + "<br>" +  "obtain Marks" 
+    + marks_obtained + "<br>" 
+    + " Grade : your grade is A " + "<br>" + " Remark :you are good" 
+    + "<br>" + "Your Pecentage is " + percentage)}
+
+ 
+
+
+
